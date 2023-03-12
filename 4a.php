d4b803b2539be50160d373d4e73ad2c6e93fea07
Nayara Christony Paul Barriga <92663332+SolidNoob502@users.noreply.github.com>
2023-03-12 13:39:31 +0700

    Add files via upload

---
+++ 4a.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 4a</title>
+</head>
+<body>
+    
+    <?php 
+    
+        $words = ['epan', 'cipta', 'akan', 'u', 'asa', 'esok'];
+        $phrase1 = "kan";
+        $phrase2 = "oleh apa yang kau kerj";
+        $phrase3 = "hari ini";
+        $space = ' ';
+
+        
+        echo 'M' . $words[4] . $space . 'd' . $words[0] . 'mu di' . $words[1] . $phrase1 . $space . $phrase2 . $words[2] . $space . $phrase3 . ',' . $space . 'b' . $words[3] . $phrase1 . $space . 'b' . $words[5];
+    
+    ?>
+
+</body>
+</html>
